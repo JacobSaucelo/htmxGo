71d2966cafa7df17f402ef5a2087c30b98da89fa
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-15 18:46:21 +0800

    range on pets

--- main.go
+++ main.go
@@ -6,11 +6,25 @@ import (
 	"text/template"
 )
 
+type Pet struct {
+	PetType string
+	Color   string
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		page := template.Must(template.ParseFiles("index.html"))
-		page.Execute(w, nil)
+
+		pets := map[string][]Pet{
+			"Pets": {
+				{PetType: "Cat", Color: "orange"},
+				{PetType: "Dog", Color: "black"},
+				{PetType: "Cat", Color: "orange & white"},
+			},
+		}
+
+		page.Execute(w, pets)
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
