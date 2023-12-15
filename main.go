fb968f93bb4b1162c995b50612ed9ec6490cdfd2
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-15 18:29:35 +0800

    added htmx

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, r.Method)
+		io.WriteString(w, r.Method)
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
